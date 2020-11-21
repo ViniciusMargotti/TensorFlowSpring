652cafc10a2ecadc455b2d72b863556092df244d
Vinícius Margotti <viniciusmargotti@hotmail.com>
2020-11-21 12:04:07 -0300

    alterando gitignore

--- src/main/java/edu/ml/tensorflow/service/storage/StorageProperties.java
+++
@@ -1,7 +0,0 @@
-package edu.ml.tensorflow.service.storage;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("storage")
-public class StorageProperties {
-}
---
+++ src/main/java/vinicius/ml/tensorflow/service/storage/StorageProperties.java
@@ -0,0 +1,7 @@
+package vinicius.ml.tensorflow.service.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+}
